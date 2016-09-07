ae0de1ef1b253f797041541b7d9ddc856ad6be1a
joongs <rlatpwnd8314@naver.com>
2016-09-08 00:00:39 +0900

    Create base_code_02.java

---
+++ base_code_02.java
@@ -0,0 +1,29 @@
+/*
+ * 2장 시작 기본 코드 구조
+ * */
+
+import junit.framework.TestCase;
+
+public class MoneyTest extends TestCase{
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+		
+		five.times(3);
+		assertEquals(15, five.amount);
+	}
+}
+
+class Dollar{
+	//int amount = 10;
+	int amount;
+	
+	Dollar (int amount) {
+		this.amount = amount;
+	}
+	
+	void times(int multiplier){
+		amount = amount * multiplier;
+	}
+}
