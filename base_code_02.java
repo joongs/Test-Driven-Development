34d82f647c8fc12276237954a8c2a00673ae50f1
joongs <rlatpwnd8314@naver.com>
2016-10-17 23:29:30 +0900

    Update base_code_02.java

--- base_code_02.java
+++ base_code_02.java
@@ -2,6 +2,7 @@
  * 2장 시작 기본 코드 구조
  * */
 
+// MoneyTest.java
 import junit.framework.TestCase;
 
 public class MoneyTest extends TestCase{
@@ -15,6 +16,7 @@ public class MoneyTest extends TestCase{
 	}
 }
 
+// Dollar.java
 class Dollar{
 	//int amount = 10;
 	int amount;
